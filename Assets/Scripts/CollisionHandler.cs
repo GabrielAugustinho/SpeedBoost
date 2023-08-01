27ca99baecfed62a9c0c6d1faf57ce761980d1e2
Gabriel Augustinho Alves <gabriel.aug.alves@outlook.com>
2023-08-01 10:44:15 -0300

    Transicao de fases

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,23 +3,42 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 0.8f;
     void OnCollisionEnter(Collision other)
     {
         switch(other.gameObject.tag)
-        {
-            case "Finish":
-                Debug.Log("Acabou o jogo");
-                break;
+        {           
             case "Friendly":
                 Debug.Log("Começou o jogo");
                 break;
-            case "Fuel":
-                Debug.Log("Combustivel");
-                break;            
+            case "Finish":
+                StartSuccessSequence();
+                break;          
             default:
-                ReloadLevel();                
+                StartCrashSequence();
                 break;
         }                
+    }
+
+    void StartSuccessSequence(){
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void LoadNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
     void ReloadLevel()
