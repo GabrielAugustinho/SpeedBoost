19c6fb4479dd35d86317493e18435a125890721a
Gabriel Augustinho Alves <gabriel.aug.alves@outlook.com>
2023-08-03 17:03:09 -0300

    Adicao de esc para sair do jogo

---
+++ Assets/Scripts/QuitApplicationHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplicationHandler : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("Clicamos em ESC");
+        }
+    }
+}
